4dec087ad7ed814daef84fa3a55b29b91c3d4693
Amanuel Mekashaw <amanuelmekashaw29@gmail.com>
2024-11-12 10:46:11 +0300

    inital commit

---
+++ src/main/java/com/project/bookreviewapp/BookreviewappApplication.java
@@ -0,0 +1,13 @@
+package com.project.bookreviewapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookreviewappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookreviewappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/bookreviewapp/BookreviewappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bookreviewapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookreviewappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
