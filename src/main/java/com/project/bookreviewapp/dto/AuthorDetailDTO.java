e4be74c1584c23e33039201797be1db4ecc45a03
Amanuel Mekashaw <amanuelmekashaw29@gmail.com>
2024-11-12 16:50:58 +0300

    added AuthorDetailDTO

---
+++ src/main/java/com/project/bookreviewapp/dto/AuthorDetailDTO.java
@@ -0,0 +1,26 @@
+package com.project.bookreviewapp.dto;
+
+import java.util.List;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class AuthorDetailDTO {
+    @NotNull(message = "User ID cannot be null")
+    private Long id;
+
+    @Size(max = 1000, message = "Biography must be less than or equal to 500 characters")
+    private String biography;
+
+    @Size(max = 5, message = "There can be a maximum of 5 social links")
+    private List<@Pattern(regexp = SocialLinkValidator.URL_PATTERN, message = "Invalid URL format for social link") String> socialLinks;
+
+    public static class SocialLinkValidator {
+        // Constant for the URL pattern validation
+        public static final String URL_PATTERN = "^(https?://)?(www\\.)?[a-zA-Z0-9-]+\\.[a-zA-Z]{2,}(\\/.*)?$";
+    }
+
+}
