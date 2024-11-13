9b847147e11e8a246885b1a5754b1a95da8c0737
Amanuel Mekashaw <amanuelmekashaw29@gmail.com>
2024-11-13 13:16:21 +0300

    Application configuration for JWT

---
+++ src/main/java/com/project/bookreviewapp/config/ApplicationConfig.java
@@ -0,0 +1,23 @@
+package com.project.bookreviewapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.project.bookreviewapp.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return (username) -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("user not found"));
+    }
+}
