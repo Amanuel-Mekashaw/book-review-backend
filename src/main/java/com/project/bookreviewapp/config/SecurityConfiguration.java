5400027e637595e0f7a3024609c8c7a2d9fc5ccc
Amanuel Mekashaw <amanuelmekashaw29@gmail.com>
2024-11-16 09:36:52 +0300

    Genre controller DTO and mapper has seen their final, added the flashpost api json export since i hate setting up everything, updated Author detail post request with added validation

--- src/main/java/com/project/bookreviewapp/config/SecurityConfiguration.java
+++ src/main/java/com/project/bookreviewapp/config/SecurityConfiguration.java
@@ -16,25 +16,19 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
-        private final AuthenticationProvider authenticationProvider;
-        private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+    private final JwtAuthenticationFilter jwtAuthFilter;
 
-        @Bean
-        SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-                return http.csrf(csrf -> csrf
-                                .disable())
-                                .authorizeHttpRequests(requests -> requests
-                                                .requestMatchers("/api/v1/auth/**", "/api/v1/genre/**")
-                                                // .requestMatchers("/error")
-                                                .permitAll()
-                                                .anyRequest()
-                                                .authenticated())
-                                .sessionManagement(session -> session
-                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                                .authenticationProvider(authenticationProvider)
-                                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-                                .build();
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(requests -> requests.requestMatchers("/api/v1/auth/**")
+                        // .requestMatchers("/error")
+                        .permitAll().anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class).build();
 
-        }
+    }
 
 }
--- src/main/java/com/project/bookreviewapp/mapper/GenreMapper.java
+++ src/main/java/com/project/bookreviewapp/mapper/GenreMapper.java
@@ -5,17 +5,11 @@ import com.project.bookreviewapp.entity.Genre;
 
 public class GenreMapper {
 
-    public static GenreDTO genreDTOToGenre(Genre genre) {
-        return GenreDTO.builder()
-                .id(genre.getId())
-                .name(genre.getName())
-                .build();
+    public static GenreDTO genreToGenreDTO(Genre genre) {
+        return GenreDTO.builder().id(genre.getId()).name(genre.getName()).build();
     }
 
-    public static Genre genreToGenreDTO(GenreDTO genre) {
-        return Genre.builder()
-                .id(genre.getId())
-                .name(genre.getName())
-                .build();
+    public static Genre genreDTOToGenre(GenreDTO genreDTO) {
+        return Genre.builder().id(genreDTO.getId()).name(genreDTO.getName()).build();
     }
 }
